caf90250de577b511b59b48691ac563fa8c2fcea
Leticia Rodrigues <leticiaarodrigues2@gmail.com>
2021-11-25 22:50:14 -0300

    Componente mine

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import React , {Component} from "react";
 import { StyleSheet, Text, View } from "react-native";
 import params from "./src/params";
-import Field from './src/components/Feld'
+import Field from "./src/components/Field";
+
 
 export default class App extends Component {
 render(){
   return(
-    <View>
-    <Text> INIICIANDO MINES</Text>
-    <Text> Tamanho da grade :
-      {params.getRowsAmount()}x{params.getColumnsAmount()}
-    </Text>
-    <Field/>
-    <Field/>
-    <Field opened nearMines ={1}/>
-    <Field opened nearMines ={2}/>
-    <Field opened nearMines ={3}/>
-    <Field opened nearMines ={6}/>
-
-
+    <View style = {styles.container}>
+      <Text style = {styles.welcome}> INIICIANDO MINES</Text>
+      <Text style = {styles.instructions}> Tamanho da grade :
+        {params.getRowsAmount()}x{params.getColumnsAmount()}
+      </Text>
+      
+      <Field/>
+      <Field/>
+      <Field opened nearMines ={1}/>
+      <Field opened nearMines ={2}/>
+      <Field opened nearMines ={3}/>
+      <Field opened nearMines ={6}/>
+      <Field mined />
+      <Field mined opened />
+      <Field mined opened exploded />
 
     </View>
   );
@@ -28,15 +31,19 @@ render(){
 
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#F5FCFF',
+
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  welcome: {
+    fontSize: 20,
+    textAlign:'center',
+    margin:10,
   },
-  sectionDescription: {
+  instructions: {
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
